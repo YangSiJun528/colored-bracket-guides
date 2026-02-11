642dc60e11501c8da9d919de9d9e057ab4b73abd
GitHub Action <action@github.com>
2026-02-11 09:36:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/yangsijun528/coloredbracketguides/MyBundle.kt
+++ src/main/kotlin/com/github/yangsijun528/coloredbracketguides/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.yangsijun528.coloredbracketguides
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/yangsijun528/coloredbracketguides/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/yangsijun528/coloredbracketguides/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.yangsijun528.coloredbracketguides.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
